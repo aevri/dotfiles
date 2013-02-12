642648fc064eeb88a6e0ae8affb7cfc3d3780d2c
aevri <aevri@server.fake>
2013-02-12 12:04:47 +0000

    initial commit

---
+++ get-pathogen.sh
@@ -0,0 +1,1 @@
+curl https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim > vim/vimdir/autoload/pathogen.vim
---
+++ make-command-t.sh
@@ -0,0 +1,4 @@
+sudo apt-get install ruby1.9.1-dev
+cd vim/vimdir/bundle/command-t/ruby/command-t
+ruby extconf.rb
+make
