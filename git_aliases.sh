9673b5330bef1b3e9da8335484a2301897f0cb23
Angelos Evripiotis <aevri@users.noreply.github.com>
2014-11-18 16:40:07 +0000

    git_aliases: add 'headmasters' alias
    Add a 'git headmasters' alias which will show the logdog of commits
    unique to HEAD and all the master branches in all the remotes.

--- git_aliases.sh
+++ git_aliases.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/env bash
 git config --global alias.logdog "log --decorate --oneline --graph"
 git config --global alias.sed '! '"git ls-files -z | xargs --null sed --in-place --expression"
+git config --global alias.headmasters 'logdog HEAD $(for remote in $(git remote); do echo -n "$remote/master "; done) --not $(git merge-base --octopus HEAD $(for remote in $(git remote); do echo -n "$remote/master "; done))~1'
