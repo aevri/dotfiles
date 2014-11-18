919a7c3962a3ac76c9dc065e442d5bfd8ecd436a
Angelos Evripiotis <aevri@users.noreply.github.com>
2014-11-18 17:05:07 +0000

    git_aliases: fix 'headmasters' alias

--- git_aliases.sh
+++ git_aliases.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 git config --global alias.logdog "log --decorate --oneline --graph"
 git config --global alias.sed '! '"git ls-files -z | xargs --null sed --in-place --expression"
-git config --global alias.headmasters 'logdog HEAD $(for remote in $(git remote); do echo -n "$remote/master "; done) --not $(git merge-base --octopus HEAD $(for remote in $(git remote); do echo -n "$remote/master "; done))~1'
+git config --global alias.headmasters '!git logdog HEAD $(for remote in $(git remote); do echo -n "$remote/master "; done) --not $(git merge-base --octopus HEAD $(for remote in $(git remote); do echo -n "$remote/master "; done))~1'
