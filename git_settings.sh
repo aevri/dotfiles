d5f62c6d87790ff5954b9094d81e88f17cd14e09
Angelos Evripiotis <angelos.evripiotis@gmail.com>
2015-07-26 13:39:38 +0100

    git_settings.sh: use push.default simple
    'Squelch' warning messages when using older versions of Git.

--- git_settings.sh
+++ git_settings.sh
@@ -2,3 +2,4 @@
 git config --global merge.conflictstyle diff3
 git config --global user.name "Angelos Evripiotis"
 git config --global user.email angelos.evripiotis@gmail.com
+git config --global push.default simple
