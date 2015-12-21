11cf14d8cfcdf7073e117d3a624e74bf720edc3d
Angelos Evripiotis <angelos.evripiotis@gmail.com>
2015-12-21 10:05:37 +0000

    git_aliases.sh: use 'printf' instead of 'echo -n'
    It seems that on Mac OSX, the default 'echo' doesn't understand '-n'.
    Use 'printf' instead, as that seems to be available.

--- git_aliases.sh
+++ git_aliases.sh
@@ -3,6 +3,6 @@ git config --global alias.logdog "log --decorate --oneline --graph"
 git config --global alias.logdogtoday "log --decorate --oneline --graph --all --since midnight"
 git config --global alias.logdog5 "log --decorate --oneline --graph -5"
 git config --global alias.sed '! '"git ls-files -z | xargs --null sed --in-place --expression"
-git config --global alias.headmasters '!git logdog HEAD $(for remote in $(git remote); do echo -n "$remote/master "; done) --not $(git merge-base --octopus HEAD $(for remote in $(git remote); do echo -n "$remote/master "; done))~1'
+git config --global alias.headmasters '!git logdog HEAD $(for remote in $(git remote); do printf "$remote/master "; done) --not $(git merge-base --octopus HEAD $(for remote in $(git remote); do printf "$remote/master "; done))~1'
 git config --global alias.dupe '!git ls-tree -r HEAD | cut -c 13- | sort | uniq -D -w 40'
 git config --global alias.prfetch '!f() { git fetch $1 +refs/pull/*:refs/pull/* ; }; f'
