515694ef1c4841df1282241c7acaa4e078db826a
Angelos Evripiotis <angelos.evripiotis@gmail.com>
2018-11-30 17:42:43 +0400

    git_aliases: add 'addp'

--- git_aliases.sh
+++ git_aliases.sh
@@ -6,3 +6,5 @@ git config --global alias.sed '! '"git ls-files -z | xargs --null sed --in-place
 git config --global alias.headmasters '!git logdog HEAD $(for remote in $(git remote); do printf "$remote/master "; done) --not $(git merge-base --octopus HEAD $(for remote in $(git remote); do printf "$remote/master "; done))~1'
 git config --global alias.dupe '!git ls-tree -r HEAD | cut -c 13- | sort | uniq -D -w 40'
 git config --global alias.prfetch '!f() { git fetch $1 +refs/pull/*:refs/pull/* ; }; f'
+
+git config --global alias.addp '!git -c interactive.diffFilter="/usr/local/share/git-core/contrib/diff-highlight/diff-highlight | less -FRX --tabs=4" add -p'
