f1bacdc1d96a9f0894114e4633c13b72e4d69d0d
Angelos Evripiotis <jevripiotis@bloomberg.net>
2019-10-03 09:29:35 +0100

    git_aliases: lots of new aliases, incl 'fshow'

--- git_aliases.sh
+++ git_aliases.sh
@@ -6,5 +6,12 @@ git config --global alias.sed '! '"git ls-files -z | xargs --null sed --in-place
 git config --global alias.headmasters '!git logdog HEAD $(for remote in $(git remote); do printf "$remote/master "; done) --not $(git merge-base --octopus HEAD $(for remote in $(git remote); do printf "$remote/master "; done))~1'
 git config --global alias.dupe '!git ls-tree -r HEAD | cut -c 13- | sort | uniq -D -w 40'
 git config --global alias.prfetch '!f() { git fetch $1 +refs/pull/*:refs/pull/* ; }; f'
-
+git config --global alias.mrfetch '!f() { git fetch $1 +refs/merge-requests/*:refs/merge-requests/* ; }; f'
+git config --global alias.ombdiff '!f() { git range-diff origin/master @{u} @ $@; }; f'
+git config --global alias.umbdiff '!f() { git range-diff upstream/master @{u} @ $@; }; f'
+git config --global alias.omrebase '!f() { ancestor=$(git merge-base origin/master @) || return 1; git rebase  -i $ancestor --autosquash; }; f'
+git config --global alias.umrebase '!f() { ancestor=$(git merge-base upstream/master @) || return 1; git rebase  -i $ancestor --autosquash; }; f'
+git config --global alias.pushfl 'push --force --force-with-lease'
 git config --global alias.addp '!git -c interactive.diffFilter="/usr/local/share/git-core/contrib/diff-highlight/diff-highlight | less -FRX --tabs=4" add -p'
+git config --global alias.fshow '!git -c interactive.diffFilter="/usr/local/share/git-core/contrib/diff-highlight/diff-highlight | less -FRX --tabs=4" show $@'
+git config --global alias.fdiff '!git -c interactive.diffFilter="/usr/local/share/git-core/contrib/diff-highlight/diff-highlight | less -FRX --tabs=4" diff $@'
